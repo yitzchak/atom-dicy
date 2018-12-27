3d4783c281807a6da8bcb3538355e18acf8f2c0b
Tarn Burton <twburton@gmail.com>
2018-12-27 09:56:15 -0500

    Update for v0.1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,8 +3,17 @@
 All significant changes to this project will be documented in the notes below.
 This project adheres to [Semantic Versioning](http://semver.org/).
 
+## [v0.1.1][] — 2018-12-27
+
+### Fixed
+
+- Updated DiCy to fix hashing bug from breaking busy indicator.
+
+
 ## [v0.1.0][] — 2018-11-05
 
 Initial release.
 
-[v0.1.0]: https://github.com/yitzchak/dicy/tree/v0.1.0
+[v0.1.1]: https://github.com/yitzchak/atom-dicy/compare/v0.1.0...v0.1.1
+
+[v0.1.0]: https://github.com/yitzchak/atom-dicy/tree/v0.1.0
--- lib/Nexus.ts
+++ lib/Nexus.ts
@@ -8,7 +8,7 @@ const url2path = require('file-uri-to-path')
 
 import { Opener, OpenerFeature } from './Opener'
 
-const SCOPE_PATTERN = /^(text\.tex(?:\.latex|\.latex\.knitr|\.latex\.haskell|\.latex\.agda)?|source\.pweave\..*|source\.metys\..*)$/
+const SCOPE_PATTERN = /^(text\.tex(?:\.latex|\.latex\.knitr|\.latex\.haskell|\.latex\.agda)?|source\.pweave\..*|source\.metys.*|source\.latex)$/
 
 function editorIsLaTeX (editor: TextEditor | undefined): boolean {
   return !!editor && SCOPE_PATTERN.test(editor.getRootScopeDescriptor().getScopesArray().join(''))
